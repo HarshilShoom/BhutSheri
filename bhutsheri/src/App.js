74f03a7070467ef44be64717630a4349f56d8f10
HarshilShoom <harshilshoom6@gmail.com>
2021-11-16 07:29:30 -0500

    Update App.js
    Add Import statement to add libraries to project.

--- bhutsheri/src/App.js
+++ bhutsheri/src/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// imports bootstrap CSS File
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+// Custom Components import.
 
 class App extends Component{
   render(){
@@ -6,6 +13,7 @@ class App extends Component{
       <React.Fragment>
         <div>
           <h1>Welcome to BhutSheri</h1>
+          <Button variant="primary">HI</Button>
         </div>
       </React.Fragment>
     );
